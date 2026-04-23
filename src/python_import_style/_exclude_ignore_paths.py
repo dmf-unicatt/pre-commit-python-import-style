1ae952a9c34a8d6959e937a3968753d5c1905494
Francesco Ballarin <francesco.ballarin@unicatt.it>
2026-04-23 12:08:05 +0200

    Revise _exclude_ignore_paths to better handle return codes

--- src/python_import_style/_exclude_ignore_paths.py
+++ src/python_import_style/_exclude_ignore_paths.py
@@ -16,16 +16,17 @@ def _exclude_ignore_paths(
     assert all(p.is_absolute() for p in paths)
     rel_paths = [p.relative_to(project_root) for p in paths]
     input_bytes = "\0".join(str(p) for p in rel_paths).encode("utf-8")
+    print(rel_paths, input_bytes)
     proc = subprocess.run(
         ["git", "check-ignore", "-z", "--stdin"],
         input=input_bytes,
         cwd=str(project_root),
         stdout=subprocess.PIPE,
         stderr=subprocess.DEVNULL,
-        check=True,
+        check=False,
     )
     ignored: set[pathlib.Path] = set()
-    if proc.stdout:
+    if proc.returncode == 0:  # One or more of the provided paths is ignored
         for raw in proc.stdout.strip(b"\0").split(b"\0"):
             ignored_rel_path = pathlib.Path(raw.decode("utf-8"))
             assert ignored_rel_path in rel_paths, (
@@ -35,4 +36,13 @@ def _exclude_ignore_paths(
                 f"Git ignored path does not exist: {ignored_rel_path}"
             )
             ignored.add(ignored_rel_path)
+    elif proc.returncode == 1:  # None of the provided paths are ignored.
+        pass
+    else:
+        stdout = proc.stdout.decode("utf-8") if proc.stdout else "empty"
+        stderr = proc.stderr.decode("utf-8") if proc.stderr else "empty"
+        raise RuntimeError(
+            f"Git check-ignore failed with return code {proc.returncode}, "
+            f"stdout: {stdout} and stderr: {stderr}"
+        )
     return [project_root / p for p in rel_paths if p not in ignored]
--- tests/test_exclude_ignore_paths.py
+++ tests/test_exclude_ignore_paths.py
@@ -8,6 +8,8 @@
 import pathlib
 import subprocess
 
+import pytest
+
 import python_import_style._exclude_ignore_paths
 
 
@@ -30,3 +32,33 @@ def test_exclude_ignore_paths(
     assert python_import_style._exclude_ignore_paths._exclude_ignore_paths(
         tmp_path, [mod1, mod2]
     ) == [mod1]
+
+
+def test_exclude_ignore_paths_no_gitignore(
+    tmp_path: pathlib.Path,
+) -> None:
+    """Test that no files are ignored if there is no .gitignore file."""
+    mod1 = tmp_path / "mod1.py"
+    mod1.write_text("# mock file 1\n", encoding="utf-8")
+    subprocess.run(
+        ["git", "init"],
+        cwd=str(tmp_path),
+        check=True,
+    )
+
+    assert python_import_style._exclude_ignore_paths._exclude_ignore_paths(
+        tmp_path, [mod1]
+    ) == [mod1]
+
+
+def test_exclude_ignore_paths_no_git_repository(
+    tmp_path: pathlib.Path,
+) -> None:
+    """Test that an error is raised if there is no git repository."""
+    mod1 = tmp_path / "mod1.py"
+    mod1.write_text("# mock file 1\n", encoding="utf-8")
+
+    with pytest.raises(RuntimeError):
+        python_import_style._exclude_ignore_paths._exclude_ignore_paths(
+            tmp_path, [mod1]
+        )
